f30ee5f38ce9dbd19a5a715e0a183fa5d54ae179
indaKim <pinktail2327@naver.com>
2022-08-14 21:08:11 +0900

    background image url 수정

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const bgImages = ["img/01.jpg", "img/02.jpg", "img/03.jpg","img/04.jpg","img/05.
 const chosenBgImages = bgImages[Math.floor(Math.random() * bgImages.length)];
 
 const body = document.querySelector("body");
-body.style.backgroundImage = "url('"+ chosenBgImages + "')";
+body.style.backgroundImage = "url(" + chosenBgImages + ")";
